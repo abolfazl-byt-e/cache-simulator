10c2c633ae6bc284fb23d92b33128cc1d087c8cd
abolfazlbyte <abolfazl.byte@gmail.com>
2023-11-13 04:38:18 +0330

    add initial code to write this simulator

---
+++ cache-simulator.py
@@ -0,0 +1,11 @@
+# simulate a cache and calculate hit ratio of some replacement policy
+
+#TODO create cache with some specification
+
+#TODO read PC and ADDRESS from benchmarks
+
+#TODO LRU policy function
+
+#TODO NMRU policy function
+
+#TODO Random policy function
